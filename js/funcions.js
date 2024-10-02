82d42656607811bb7a4d06fa7f0842b2b6e3bd56
Leonardo Edenak <leonardoedenak@gmail.com>
2024-10-02 10:32:02 +0200

    .

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,10 @@
 function confirmaRegistre(){
     //completa
+    alert("sexo")
+    console.log("siu")
+    let div = document.getElementById("formDiv");
+    div.innerHTML = "<p class='important'>Hacido</p>"
+    return false
 }
+
+//confirmaRegistre()
